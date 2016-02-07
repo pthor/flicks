e5808a78e7ae836d8d46b77e8e16f0874a7a7e11
Paul Thormahlen <pauldavid@gmail.com>
2016-02-07 13:09:46 -0800

    Add search q!

--- Flicks/Movie.swift
+++ Flicks/Movie.swift
@@ -11,24 +11,49 @@ import AFNetworking
 //private let params = ["api-key": "53eb9541b4374660d6f3c0001d6249ca:19:70900879"]
 private let apiKey = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
 private let nowPlyaingUrl = "https://api.themoviedb.org/3/movie/now_playing?api_key=\(apiKey)"
+private let searchMoviesUrl = "http://api.themoviedb.org/3/search/movie?api_key=\(apiKey)"
+
 
 class Movie {
     var title: String?
     var overview: String?
     var posterPath: String?
     var backdropPath: String?
+    var rating: Double?
     
     init(jsonResult: NSDictionary) {
-        print(jsonResult["title"])
         title = jsonResult["title"] as? String
         overview = jsonResult["overview"] as? String
         posterPath = jsonResult["poster_path"] as? String
         backdropPath = jsonResult["backdrop_path"] as? String
+        rating = jsonResult["vote_average"] as? Double
+        print(rating)
     }
     
     class func fetchNowPlaying(successCallback: ([Movie]) -> Void, error: ((NSError?) -> Void)?) {
         let manager = AFHTTPRequestOperationManager()
         manager.GET(nowPlyaingUrl, parameters: [], success: { (operation ,responseObject) -> Void in
+            if let results = responseObject["results"] as? NSArray {
+                var movies: [Movie] = []
+                for result in results as! [NSDictionary] {
+                    movies.append(Movie(jsonResult: result))
+                }
+                successCallback(movies)
+            }
+            }, failure: { (operation, requestError) -> Void in
+                if let errorCallback = error {
+                    errorCallback(requestError)
+                }
+        })
+    }
+    
+    class func searchMovies(searchText:String,successCallback: ([Movie]) -> Void,
+        error: ((NSError?) -> Void)?) {
+        let queryParam = searchText.stringByAddingPercentEncodingWithAllowedCharacters(.URLHostAllowedCharacterSet())!
+        let searchUrl = searchMoviesUrl + "&query=\(queryParam)"
+            print(searchUrl)
+        let manager = AFHTTPRequestOperationManager()
+        manager.GET(searchUrl, parameters: [], success: { (operation ,responseObject) -> Void in
             if let results = responseObject["results"] as? NSArray {
                 var movies: [Movie] = []
                 for result in results as! [NSDictionary] {
--- Flicks/ViewController.swift
+++ Flicks/ViewController.swift
@@ -10,23 +10,29 @@ import UIKit
 import AFNetworking
 import MBProgressHUD
 
-class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, UISearchBarDelegate {
     
     private var movies = [Movie]()
 
     @IBOutlet weak var moviesTableView: UITableView!
     @IBOutlet weak var networkErrorNotificationView: UIView!
     
+    @IBOutlet weak var movieSearchBar: UISearchBar!
+    var searchActive : Bool = false
+    
     let posterBaseUrl = "http://image.tmdb.org/t/p/w780"
     let posterSmallBaseUrl = "http://image.tmdb.org/t/p/w92"
+    let movieRefreshControl = UIRefreshControl()
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         networkErrorNotificationView.hidden = true
-        moviesTableView.rowHeight = 250.0
-        let refreshControl = UIRefreshControl()
-        refreshControl.addTarget(self, action: "refrechMoviesFromNetowrk:", forControlEvents: UIControlEvents.ValueChanged)
-        moviesTableView.insertSubview(refreshControl, atIndex: 0)
+        movieSearchBar.delegate = self
+        moviesTableView.rowHeight = 200.0
+        self.networkErrorNotificationView.hidden = true
+        movieRefreshControl.addTarget(self, action: "refetchMoviesFromNetowrk:", forControlEvents: UIControlEvents.ValueChanged)
+        moviesTableView.insertSubview(movieRefreshControl, atIndex: 0)
         loadMoviesFromNetowrk()
         
     }
@@ -35,15 +41,40 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         return movies.count
     }
     
+    func scrollViewWillBeginDragging(scrollView: UIScrollView) {
+        movieSearchBar.resignFirstResponder()
+        print("hide the keyboard yo")
+    }
+    
+    func tableView(tableView: UITableView, didselectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = self.moviesTableView.cellForRowAtIndexPath(indexPath) as! MyCell
+        cell.blurEffectView.alpha = 1.0
+    }
+    
+    func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        let cell = self.moviesTableView.cellForRowAtIndexPath(indexPath) as! MyCell
+        cell.blurEffectView.alpha = 0.8
+    }
+    
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier(
         "com.codepath.exampleCell")! as! MyCell
         //clear out any old junk
         cell.postImageView.image = nil
+        cell.postThumbnailView.image = nil
         let movie = movies[indexPath.row]
-        if let posterPath = movie.backdropPath{
-            //let posterUrl = posterBaseUrl + posterPath
-            loadImageForCell(cell,posterPath:  posterPath)
+        if let backdropPath = movie.backdropPath{
+            loadImageForCell(cell.postImageView,posterPath: backdropPath)
+        }
+        if let posterPath = movie.posterPath{
+            loadImageForCell(cell.postThumbnailView,posterPath: posterPath)
+        }
+        print(" set cell rating = \(movie.rating) out of 1-")
+        if let rating = movie.rating {
+            cell.ratingLabel.text = "Rating: \(rating)/10"
+        }
+        else{
+            cell.ratingLabel.text = ""
         }
         cell.myCustomLabel.text = "\(movie.title!)"
         return cell
@@ -56,94 +87,106 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let movie = movies[indexPath!.row]
         let detailViewController = segue.destinationViewController as! DetailViewController
         detailViewController.movie = movie
-        print("prepare for seque")
     }
     
-    func loadImageForCell(cell: MyCell, posterPath: String){
+    func loadImageForCell(myImageView: UIImageView, posterPath: String){
         
         let smallImageRequest = NSURLRequest(URL: NSURL(string: posterSmallBaseUrl + posterPath)!)
         let largeImageRequest = NSURLRequest(URL: NSURL(string: posterBaseUrl + posterPath)!)
-        let myImageView = cell.postImageView
         
         myImageView.setImageWithURLRequest(
             smallImageRequest,
             placeholderImage: nil,
             success: { (smallImageRequest, smallImageResponse, smallImage) -> Void in
-                
-                // smallImageResponse will be nil if the smallImage is already available
-                // in cache (might want to do something smarter in that case).
                 myImageView.alpha = 0.0
                 myImageView.image = smallImage;
                 
                 UIView.animateWithDuration(0.3, animations: { () -> Void in
-                    
                     myImageView.alpha = 1.0
                     
                     }, completion: { (sucess) -> Void in
-                        
-                        // The AFNetworking ImageView Category only allows one request to be sent at a time
-                        // per ImageView. This code must be in the completion block.
                         myImageView.setImageWithURLRequest(
                             largeImageRequest,
                             placeholderImage: smallImage,
                             success: { (largeImageRequest, largeImageResponse, largeImage) -> Void in
-                                
                                 myImageView.image = largeImage;
                                 
                             },
                             failure: { (request, response, error) -> Void in
-                                self.networkErrorNotificationView.hidden = false
+                                //this is causing issues when it shouldnt
+                                //self.networkErrorNotificationView.hidden = false
                         })
                 })
             },
             failure: { (request, response, error) -> Void in
-                self.networkErrorNotificationView.hidden = false
+                //this is causing issues when it shouldnt
+                //self.networkErrorNotificationView.hidden = false
         })
     }
     
     func loadMoviesFromNetowrk()
     {
-        // Display HUD right before the request is made
-        let hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
-        hud.labelText = "Fetching flicks";
-        Movie.fetchNowPlaying({ (movies) -> Void in
-              self.movies = movies
-              print("\(movies)")
-              //sad I have to put in a delay just to show the progress indicator
-              sleep(1)
-              MBProgressHUD.hideHUDForView(self.view, animated: true)
-              self.moviesTableView.reloadData()
-            })
-            { (error) -> Void in
-                sleep(1)
-                MBProgressHUD.hideHUDForView(self.view, animated: true)
-                self.networkErrorNotificationView.hidden = false
-
-                print("Error getting movies")
-            }
+        refetchMoviesFromNetowrk(movieRefreshControl)
     }
     
-    func refrechMoviesFromNetowrk(refreshControl: UIRefreshControl)
+    func onMovieFetchError(error: NSError?) -> Void{
+        print("Error getting movies")
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+        self.networkErrorNotificationView.hidden = false
+    }
+    
+    func onMovieFetchSuccess(movies: [Movie]) -> Void{
+        self.movies = movies
+        MBProgressHUD.hideHUDForView(self.view, animated: true)
+        self.networkErrorNotificationView.hidden = true
+        self.movieRefreshControl.endRefreshing()
+        self.moviesTableView.reloadData()
+        if(self.movies.count == 0){
+            searchActive = false;
+        } else {
+            searchActive = true;
+        }
+    }
+    
+    func refetchMoviesFromNetowrk(refreshControl: UIRefreshControl)
     {
         // Display HUD right before the request is made
         let hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
         hud.labelText = "Fetching flicks";
-        Movie.fetchNowPlaying({ (movies) -> Void in
-            self.movies = movies
-            print("\(movies)")
-            //sad I have to put in a delay just to show the progress indicator
-            sleep(1)
+        Movie.fetchNowPlaying(onMovieFetchSuccess, error: onMovieFetchError)
+    }
+    
+    func searchMovies(searchText:String)
+    {
+        // Display HUD right before the request is made
+        let hud = MBProgressHUD.showHUDAddedTo(self.view, animated: true)
+        hud.labelText = "Fetching flicks";
+        Movie.searchMovies(searchText, successCallback: onMovieFetchSuccess, error: onMovieFetchError)
+    }
+    
+    func searchBarTextDidBeginEditing(searchBar: UISearchBar) {
+        searchActive = true;
+    }
+    
+    func searchBarTextDidEndEditing(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarCancelButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBarSearchButtonClicked(searchBar: UISearchBar) {
+        searchActive = false;
+    }
+    
+    func searchBar(searchBar: UISearchBar, textDidChange searchText: String) {
+        print(searchText)
+        if(searchText != ""){
+            searchMovies(searchText)
             MBProgressHUD.hideHUDForView(self.view, animated: true)
-            refreshControl.endRefreshing()
-            self.moviesTableView.reloadData()
-            })
-            { (error) -> Void in
-                sleep(1)
-                MBProgressHUD.hideHUDForView(self.view, animated: true)
-                self.networkErrorNotificationView.hidden = false
-                
-                print("Error getting movies")
         }
+
     }
 
 
@@ -151,8 +194,11 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
 
 class MyCell: UITableViewCell{
 
+    @IBOutlet weak var postThumbnailView: UIImageView!
     @IBOutlet weak var postImageView: UIImageView!
     @IBOutlet weak var myCustomLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var blurEffectView: UIVisualEffectView!
 }
 
 
